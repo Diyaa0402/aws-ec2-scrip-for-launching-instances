977cee005000ab1489c7c5bfe19c685036a8c2e9
Diyaa0402 <123350612+Diyaa0402@users.noreply.github.com>
2024-07-19 15:39:53 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,13 +9,13 @@ sudo apt install nodejs
 sudo apt install npm
 
 #clone the repository
-git clone https://github.com/Diyaa0402/AWS-project-D.git
+git clone https://github.com/Diyaa0402/AWS-project-D.git/home/ubuntu/project
 
 #cd in the folder 
-cd AWS-project-D
+cd home/ubuntu/project
 
 #install packages
-npm install
+sudo npm install
 
 #turn up the server
-npx pm2 start index.js
+sudo npx pm2 start index.js
